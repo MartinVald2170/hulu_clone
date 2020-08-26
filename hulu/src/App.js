cff70f8975654954b7147f523414de3e76575c55
MartinVald2170 <noways2170@gmail.com>
2020-08-26 13:55:54 +1000

    Video Card Done

--- hulu/src/App.js
+++ hulu/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Nav from "./Components/Nav";
+import Results from "./Components/Results";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Nav />
-
-      {/* Results */}
+      <Results />
     </div>
   );
 }
---
+++ hulu/src/Components/VideoCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./VideoCard.css";
+
+function VideoCard({ movie }) {
+  return (
+    <div className="videoCard">
+      <h1>Hello</h1>
+      <img
+        src="https://th.bing.com/th/id/OIP.J38Yqq9R2Zwk_IrEPkeULQHaEL?pid=Api&rs=1"
+        alt=""
+        Likes
+      />
+      <p>This film is about garfield</p>
+      <h2>Movie Title</h2>
+      <p>Number of ...</p>
+    </div>
+  );
+}
+
+export default VideoCard;
